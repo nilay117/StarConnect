6eecc4f788ccb9f69939a51b0984b52a35651fc7
Nilay Arora <nilayarora1298@gmail.com>
2017-06-15 01:10:51 +0530

    Home Page

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// var app = angular.module('starconnect',['ngRoute'])
+// 	.config(starconnectRouter);
+
+// function starconnectRouter($routeProvider){
+// 	$routeProvider
+// 			.when('/',
+// 				{
+// 					templateUrl:'partials/destinations.html',
+// 					controller: 'appCtrl';
+// 				})
+// }
---
+++ js/controllers/app.js
@@ -0,0 +1,101 @@
+var app = angular.module("starconnect",[]);
+app.controller("ArtistCtrl",function ($scope){
+	$scope.artists = {
+		"1": {
+			"code":"1",
+			"name": "Madhur Wadhwa",
+			"subtitle":"Graphic Designer",
+			"fb":"https://www.google.com",
+			"twitter":"https://www.google.com",
+			"insta":"https://www.google.com",
+			"img":"img/mw.jpg"
+		},
+		"2": {
+			"code":"1",
+			"name": "Madhur Wadhwa",
+			"subtitle":"Graphic Designer",
+			"fb":"https://www.google.com",
+			"twitter":"https://www.google.com",
+			"insta":"https://www.google.com",
+			"img":"img/mw.jpg"
+		},
+		"3": {
+			"code":"1",
+			"name": "Madhur Wadhwa",
+			"subtitle":"Graphic Designer",
+			"fb":"https://www.google.com",
+			"twitter":"https://www.google.com",
+			"insta":"https://www.google.com",
+			"img":"img/mw.jpg"
+		},
+		"4": {
+			"code":"1",
+			"name": "Madhur Wadhwa",
+			"subtitle":"Graphic Designer",
+			"fb":"https://www.google.com",
+			"twitter":"https://www.google.com",
+			"insta":"https://www.google.com",
+			"img":"img/mw.jpg"
+		},
+		"5": {
+			"code":"1",
+			"name": "Madhur Wadhwa",
+			"subtitle":"Graphic Designer",
+			"fb":"https://www.google.com",
+			"twitter":"https://www.google.com",
+			"insta":"https://www.google.com",
+			"img":"img/mw.jpg"
+		}
+		
+	}
+});
+app.controller("CelebCtrl",function ($scope){
+	$scope.celebs = {
+		"1": {
+			"code":"1",
+			"name": "Madhur Wadhwa",
+			"subtitle":"Graphic Designer",
+			"fb":"https://www.google.com",
+			"twitter":"https://www.google.com",
+			"insta":"https://www.google.com",
+			"img":"img/mw.jpg"
+		},
+		"2": {
+			"code":"1",
+			"name": "Madhur Wadhwa",
+			"subtitle":"Graphic Designer",
+			"fb":"https://www.google.com",
+			"twitter":"https://www.google.com",
+			"insta":"https://www.google.com",
+			"img":"img/mw.jpg"
+		},
+		"3": {
+			"code":"1",
+			"name": "Madhur Wadhwa",
+			"subtitle":"Graphic Designer",
+			"fb":"https://www.google.com",
+			"twitter":"https://www.google.com",
+			"insta":"https://www.google.com",
+			"img":"img/mw.jpg"
+		},
+		"4": {
+			"code":"1",
+			"name": "Madhur Wadhwa",
+			"subtitle":"Graphic Designer",
+			"fb":"https://www.google.com",
+			"twitter":"https://www.google.com",
+			"insta":"https://www.google.com",
+			"img":"img/mw.jpg"
+		},
+		"5": {
+			"code":"1",
+			"name": "Madhur Wadhwa",
+			"subtitle":"Graphic Designer",
+			"fb":"https://www.google.com",
+			"twitter":"https://www.google.com",
+			"insta":"https://www.google.com",
+			"img":"img/mw.jpg"
+		}
+		
+	}
+});
