ea869329c8ff4120faa87e7aa0f22e86348afa86
Nilay Arora <nilayarora1298@gmail.com>
2017-09-26 04:56:45 +0530

    Error checking youtube api

--- js/controllers/artist.js
+++ js/controllers/artist.js
@@ -12,6 +12,7 @@ app.controller("artistController",function($scope,$routeParams,$window){
           //    after the API code downloads.
           var player;
           $window.onYouTubeIframeAPIReady = function() {
+            console.log($scope.currentArtist,$scope.currentArtist.connects[0]['upload_link'],$scope.currentArtist.connects[0]['upload_link'].slice(32))
             player = new YT.Player('player', {
               height: '390',
               width: '640',
