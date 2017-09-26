cea85e44e3d0df1544b9967ed457e7aa476c8e1d
Nilay Arora <nilayarora1298@gmail.com>
2017-09-26 17:33:46 +0530

    api chek

--- js/controllers/artist.js
+++ js/controllers/artist.js
@@ -2,7 +2,26 @@
 app.controller("artistController",function($scope,$routeParams,$window){
         $scope.currentArtist = $scope.artists[$routeParams.artistCode];  
 
-        
-
-      
+        jQuery(document).ready(function(){
+          //  This function creates an <iframe> (and YouTube player)
+          //    after the API code downloads.
+          var player;
+          $window.onYouTubeIframeAPIReady = function() {
+            console.log($scope.currentArtist,$scope.currentArtist.connects[0]['upload_link'],$scope.currentArtist.connects[0]['upload_link'].slice(32))
+            player = new YT.Player('player' + $scope.currentArtist.id, {
+              height: '390',
+              width: '640',
+              videoId: $scope.currentArtist.connects[0]['upload_link'].slice(32),
+              origin:"https://www.example.com",
+              events: {
+                'onReady': onPlayerReady
+              }
+            });
+          }
+          // 4. The API will call this function when the video player is ready.
+          function onPlayerReady(event) {
+            event.target.playVideo();
+          }
+        })
+    
 });
