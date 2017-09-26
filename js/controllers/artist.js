c5d56a19297287758b36fd7844972c8ef01b18ef
Megh-Thakkar <megh.1211@gmail.com>
2017-09-26 16:06:58 +0000

    ad

--- js/controllers/artist.js
+++ js/controllers/artist.js
@@ -1,11 +1,6 @@
 
 app.controller("artistController",function($scope,$routeParams,$window){
       $scope.currentArtist = $scope.artists[$routeParams.artistCode];  
-      var tag = document.createElement('script');
-
-      tag.src = "https://www.youtube.com/iframe_api";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
           //  This function creates an <iframe> (and YouTube player)
           //    after the API code downloads.
         $window.onYouTubeIframeAPIReady = function() {
