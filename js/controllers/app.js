26dc91daf7603d801457155d4e75522fc67ddbcf
Nilay Arora <nilayarora1298@gmail.com>
2017-10-15 21:10:15 +0530

    iframe load location cheange

--- js/controllers/app.js
+++ js/controllers/app.js
@@ -14,11 +14,6 @@ app.controller("AppController",function ($scope,$http,$window){
 	})
 		var artistsforvids;
 		   	//This code loads the IFrame Player API code asynchronously.
-		var tag = document.createElement('script');
-
-		tag.src = "https://www.youtube.com/iframe_api?onload=onYoutubeLoadFunction";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 	$scope.currentUser = {};
 	$scope.thankyou_data;
@@ -30,12 +25,18 @@ app.controller("AppController",function ($scope,$http,$window){
 		$scope.artists = response.data['artists'];
 		$scope.cas = response.data['ambassadors'];
 		artistsforvids = $scope.artists;
+		
+		        var tag = document.createElement('script');
 
+		tag.src = "https://www.youtube.com/iframe_api?onload=onYoutubeLoadFunction";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 		console.log("Youtube loaded!")
 	     //  This function creates an <iframe> (and YouTube player)
     	  //    after the API code downloads.
         function onYouTubeIframeAPIReady() {
+
           $.each(artistsforvids,function(i,key){
           	console.log(i,key);
             var player;
@@ -52,8 +53,7 @@ app.controller("AppController",function ($scope,$http,$window){
                 }
             }); 
           })
-      	}  
-     
+      	} 
         })
 	$(document).ready(function(){
 
