6d3de5ad376aa440292a01a4e0544c8949e063e0
Nilay Arora <nilayarora1298@gmail.com>
2017-10-17 02:05:38 +0530

    a

--- js/controllers/artist.js
+++ js/controllers/artist.js
@@ -1,14 +1,23 @@
 
 app.controller("artistController",function($scope,$rootScope,$sce,$routeParams,$window){
-    if(Object.keys($rootScope.artists).length == 0)
+    if(Object.keys($rootScope.artists).length == 0){
     	$rootScope.getData();
-    $scope.currentArtist = $rootScope.artists[$routeParams.artistCode];    
+    	init();
+    }else{
+    	init();
+    }
+    $scope.currentArtist = {};
+    $scope.url = '';
+    var init = function(){
 
+    $scope.currentArtist = $rootScope.artists[$routeParams.artistCode];    
+    	console.log($rootScope.artists,$scope.currentArtist);
   		var videourl = $scope.currentArtist.connects[0]['upload_link'].slice(32);
   		if(videourl.indexOf('&') != -1){
   			videourl = videourl.substr(0,videourl.indexOf('&'));
   		}
         $scope.url = $sce.trustAsResourceUrl("https://www.youtube.com/embed/"+videourl);
 		console.log("Youtube loaded!")
+    }
 
 });
